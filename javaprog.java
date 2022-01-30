7663abc7859c0d490a7de2deed25981af1ee5b84
root <root@ip-172-31-30-192.ec2.internal>
2022-01-30 17:14:46 +0000

    added new java file

---
+++ javaprog.java
@@ -0,0 +1,15 @@
+c class HelloWorld {
+
+  public static void main(String[] args) {
+
+	System.out.println("Hello, World");
+int a = 10;
+int b = 20;
+int c = a + b;
+System.out.println(c);
+
+  }
+
+}
+
+
